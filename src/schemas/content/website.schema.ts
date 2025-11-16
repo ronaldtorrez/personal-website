78ed918f725df7656299e7a8d66953fb9c8ae0e1
Ronald <dev@ronaldtorrez.com>
2025-11-16 19:56:59 -0400

    feat: add optional fields shortDescription and videoThumbnail to website schema
    - Enhance website schema with new optional fields

--- src/schemas/content/website.schema.ts
+++ src/schemas/content/website.schema.ts
@@ -4,8 +4,10 @@ import { reference, z } from 'astro:content'
 export const WebsiteCollectionsSchema = z.object({
 	title: StringNoNSchema,
 	description: StringNoNSchema,
+	shortDescription: StringNoNSchema.optional(),
 	date: z.coerce.date(),
 	videoId: StringNoNSchema,
+	videoThumbnail: StringNoNSchema,
 	backgroundImage: StringNoNSchema,
 	url: StringNoNSchema.url().optional(),
 	tags: z.array(StringNoNSchema).nonempty(),

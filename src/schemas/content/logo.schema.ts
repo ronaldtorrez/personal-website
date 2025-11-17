b54ff73c179282cd16f7d12d19d0088723dc6325
Ronald <dev@ronaldtorrez.com>
2025-11-16 20:07:37 -0400

    feat: update logo schema to include file path transformations
    - Changed 'file' to 'files' in LogoCollectionsSchema
    - Added path transformation for default and dark logo files

--- src/schemas/content/logo.schema.ts
+++ src/schemas/content/logo.schema.ts
@@ -2,11 +2,13 @@ import { StringNoNSchema, StringSchema } from '@schemas/string.ts'
 import { HEXADECIMAL_COLOR_REGEX } from '@utils/const.ts'
 import { z } from 'astro:content'
 
+const LogoPath = 'src/assets/logos/'
+
 export const LogoCollectionsSchema = z.object({
 	name: StringNoNSchema,
-	file: z.object({
-		default: StringNoNSchema.optional(),
-		dark: StringNoNSchema.optional()
+	files: z.object({
+		default: StringNoNSchema.transform(val => LogoPath + val).optional(),
+		dark: StringNoNSchema.transform(val => LogoPath + val).optional()
 	}),
 	colors: z.object({
 		primary: z.string().regex(HEXADECIMAL_COLOR_REGEX),
